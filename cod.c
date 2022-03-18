2a31a74412597c201bbb6ad447614966ac8eaa0f
SkyNeTz22 <skynetz_8@yahoo.ro>
2022-03-18 23:03:36 +0200

    Update cod.c
    cod.c - added code for tema1 ;  LED control through serial interface

--- cod.c
+++ cod.c
@@ -1,7 +1,51 @@
-#include <stdio.h>
+char buffer[6];
+unsigned long int valoare;
+unsigned long int copie;
+char val;
 
-int main()
+void setup()
 {
-    printf("Hello World");
-    return 0;
+	pinMode(3, OUTPUT);
+	pinMode(13, OUTPUT);
+	pinMode(5, OUTPUT);
+	pinMode(6, OUTPUT);
+	Serial.begin(9600);
+}
+
+void loop()
+{ 
+    if(Serial.available() >= 3)
+    {
+		v = Serial.read();
+		
+		if(val == '1')
+		{
+			Serial.read();
+			v=Serial.read();
+			
+			if(val == 'S')
+				digitalWrite(13, LOW);
+			
+			else if(val == 'A')
+				digitalWrite(13, HIGH);
+		}
+		
+		else if(val == '2')
+		{
+			Serial.read();
+			Serial.readBytes(buffer, 6);
+			valoare = strtoul(buffer, NULL, 16);
+			Serial.println("Am primit: ");
+			Serial.println(valoare, HEX);
+			copie = valoare & 0xFF;
+			analogWrite(3, copie);
+			valoare = valoare >> 8;
+			copie = valoare & 0xFF;
+			analogWrite(5, copie);
+			valoare = valoare >> 8;
+			copie = valoare & 0xFF;
+			analogWrite(6, copie);
+		}
+    
+    }
 }
